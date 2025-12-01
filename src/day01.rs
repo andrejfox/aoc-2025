7e84bb7dc289e8e1d8dd64ed3f6feffcfe68569b
andrejfox <andrej.vencelj.007@gmail.com>
2025-12-01 10:35:20 +0100

    Day 1 done!

---
+++ src/day01.rs
@@ -0,0 +1,89 @@
+use std::fs;
+fn get_data_vec() -> Vec<(char, u16)> {
+    fs::read_to_string("src/input.txt")
+        .expect("Error reading file")
+        .trim()
+        .parse::<String>()
+        .expect("Error parsing file to string!")
+        .split("\n")
+        .map(|line| {
+            (
+                line.get(0..1)
+                    .expect("Error getting &str at 0..1")
+                    .parse::<char>()
+                    .expect("Error parsing &str to char"),
+                line.get(1..)
+                    .expect("Error getting &str at 1..")
+                    .parse::<u16>()
+                    .expect("Error parsing &str to u16")
+            )
+        })
+        .collect::<Vec<(char, u16)>>()
+}
+
+pub fn part1() -> u16 {
+    let mut pointer: u16 = 50;
+    let mut count: u16 = 0;
+
+    for (direction, amount) in get_data_vec().iter() {
+        match direction {
+            'R' => {
+                pointer = (pointer + amount) % 100;
+            }
+            'L' => {
+                let temp: i16 = pointer as i16 - (amount % 100) as i16;
+
+                if temp >= 0 {
+                    pointer = temp as u16;
+                } else {
+                    pointer = (100 + temp) as u16;
+                }
+            }
+            _ => panic!("Invalid input char!"),
+        }
+
+        if pointer == 0 {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+pub fn part2() -> u16 {
+    let mut pointer: u16 = 50;
+    let mut count: u16 = 0;
+
+    for (direction, amount) in get_data_vec().iter() {
+        match direction {
+            'R' => {
+                count += amount / 100;
+                if pointer + amount % 100 > 100 {
+                    count += 1
+                }
+                pointer = (pointer + amount) % 100;
+            }
+            'L' => {
+                count += amount / 100;
+                let temp: i16 = pointer as i16 - (amount % 100) as i16;
+                if pointer == 0 && temp < 0{
+                    count -= 1;
+                }
+
+                if temp >= 0 {
+                    pointer = temp as u16;
+                } else {
+                    count += 1;
+                    pointer = (100 + temp) as u16;
+                }
+            }
+            _ => panic!("Invalid input char!"),
+        }
+
+        if pointer == 0 {
+            count += 1;
+        }
+    }
+
+    count
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use crate::day01::*;
+
+mod day01;
+
+fn main() {
+    println!("{}", part2());
+}
